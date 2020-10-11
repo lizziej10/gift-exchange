f276e6c9eeec4848e8abf37450ceeb9e784973ce
Liz Johnson <lizjohnson@lizs-air.lan>
2020-10-10 20:42:05 -0500

    part 1 without unit tests

---
+++ src/main/java/com/example/restservice/PairController.java
@@ -0,0 +1,31 @@
+package com.example.restservice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PairController {
+
+    private ParticipantListBuilder participantListBuilder;
+    private Map<String, String> pairs;
+
+    PairController(ParticipantListBuilder participantListBuilder) {
+        this.participantListBuilder = participantListBuilder;
+    }
+
+    @GetMapping("/pair/{id}")
+    public String getPair(@PathVariable String id) {
+        return "Your pair is: " + pairs.get(id);
+    }
+
+    @PostMapping("/pairs")
+    public Map<String, String> createPairs(@RequestBody List<String> participants) {
+        System.out.println("Executing Post.");
+        pairs = participantListBuilder.buildPairs(participants);
+        return pairs;
+    }
+}
